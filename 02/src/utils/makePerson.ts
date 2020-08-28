7438efac03bf25da616ac0bbf2b90cddc5cc8a06
whddn5464 <rlawhddn5464@naver.com>
2020-08-28 15:38:47 +0900

    type 0828

---
+++ 02/src/utils/makePerson.ts
@@ -0,0 +1,9 @@
+export function makePerson(name :string, age:number){
+    return {name :name,age:age}
+}
+export function testMakePerson(){
+    console.log(
+        makePerson('jane',22),
+        makePerson('kane',24)
+    )
+}
